98c6e9c1518318dded805db6b17115d7bc464e1d
William Whitworth <william10charles@gmail.com>
2019-11-16 12:01:58 -0700

    removed folder and added first progect

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by will whitworth on 11/16/19.
+//  Copyright © 2019 will whitworth. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
